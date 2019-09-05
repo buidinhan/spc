bf82a7ea34b36e335c7ca25c88500a3eaf0673c1
buidinhan <54894303+buidinhan@users.noreply.github.com>
2019-09-05 16:45:26 +0700

    Create date_splitting.py

---
+++ text_processing/date_splitting.py
@@ -0,0 +1,37 @@
+def split_date(string):
+    """'dd/mm/yy' -> (dd, mm, yyyy)"""
+    try:
+        portions = string.split("/")
+        day = int(portions[0])
+        month = int(portions[1])
+        year = int(portions[2])+2000
+        return day, month, year
+    except:
+        return string, "", ""
+
+
+def read_data(path):
+    dates = []
+    with open(path, "r") as f:
+        for line in f:
+            dates.append(line.strip())
+    return dates
+
+
+def output(dates, path):
+    with open(path, "w") as f:
+        f.write("original,day,month,year\n")
+        for date in dates:
+            day, month, year = split_date(date)
+            f.write("{},{},{},{}\n".format(date, day, month, year))
+
+
+def process(source, destination):
+    dates = read_data(source)
+    output(dates, destination)
+
+
+if __name__ == "__main__":
+    source = "input.txt"
+    destination = "output.csv"
+    process(source, destination)
