a9723d1c6d57bce60f7b45e77130cf115179a129
buidinhan <54894303+buidinhan@users.noreply.github.com>
2019-09-07 18:40:20 +0700

    Update spc.py

--- spc/spc.py
+++ spc/spc.py
@@ -3,19 +3,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def get_data(path):
-    df = pd.read_csv(path, header=None)
-    
+def generate_data():
+    array = np.random.randint(100, 120, (5, 10))
+    return pd.DataFrame(array)
+
+
+def add_labels(df):
     n_rows, n_cols = df.shape
     rows = ["Time {}".format(x+1) for x in range(n_rows)]
     cols = ["Measure {}".format(x+1) for x in range(n_cols)]
     df.columns = cols
     df.set_index(rows, inplace=True)
 
-    return df
-
 
 def get_mean(df):
     return df.mean(axis=1)
-
-
