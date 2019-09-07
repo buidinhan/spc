8a7886abfa4f673672b026cc9da84d06be00c4f2
buidinhan <54894303+buidinhan@users.noreply.github.com>
2019-09-07 16:58:30 +0700

    Update main.py

--- spc/main.py
+++ spc/main.py
@@ -5,6 +5,11 @@ from matplotlib import pyplot as plt
 
 def get_data(path):
     df = pd.read_csv(path, header=None)
-    n_row, n_col = df.shape
-    rows, cols = [], []
-    for i in range(n_col):
+    
+    n_rows, n_cols = df.shape
+    rows = ["Time {}".format(x+1) for x in range(n_rows)]
+    cols = ["Measure {}".format(x+1) for x in range(n_cols)]
+    df.columns = cols
+    df.set_index(rows, inplace=True)
+
+    return df
