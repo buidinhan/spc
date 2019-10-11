26c73d41097889d5358c403a67c678c3b5f7d3c4
An Bui <54894303+buidinhan@users.noreply.github.com>
2019-10-11 07:24:57 +0700

    Implement functions plot_p_chart and plot_single_measure_control_chart

--- utils/plotting.py
+++ utils/plotting.py
@@ -292,14 +292,97 @@ def plot_moving_range_chart(df, group_axis="row", x_label=None,
                        filename="moving_range_chart.png")
 
 
-def plot_p_chart():
-    # TO BE IMPLEMENTED
-    return
+def plot_p_chart(defect_col, total_col, df, x_label=None, y_label=None,
+                 title=None, ax=None, show=True, save=False, **kwargs):
+
+    proportions, p_bar, LCL, UCL = p_chart(defect_col, total_col, df)
+
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10, 6),
+                               gridspec_kw={"left": 0.07,
+                                            "right": 0.87,
+                                            "bottom": 0.1,
+                                            "top": 0.94})
+
+    group_indices = [x+1 for x in range(len(proportions))]
+    group_labels = [str(i) for i in group_indices]
+    ax.plot(group_indices, proportions, **kwargs)
+    ax.set_xticks(group_indices)
+    ax.set_xticklabels(group_labels)
+
+    ax.axhline(y=p_bar)
+    ax.annotate("p-bar={:.3f}".format(p_bar), xy=(1.01, p_bar),
+                xycoords=("axes fraction", "data"))
+
+    ax.axhline(y=LCL)
+    ax.annotate("LCL={:.3f}".format(LCL), xy=(1.01, LCL),
+                xycoords=("axes fraction", "data"))
+
+    ax.axhline(y=UCL)
+    ax.annotate("UCL={:.3f}".format(UCL), xy=(1.01, UCL),
+                xycoords=("axes fraction", "data"))
+
+    ax.set_title(title)
+
+    if x_label is None:
+        x_label = "Group No."
+
+    ax.set_xlabel(x_label)
+
+    if y_label is None:
+        y_label = "Proportion"
+
+    ax.set_ylabel(y_label)
+
+    show_and_save_plot(show=show, save=save, filename="p_chart.png")
 
 
-def plot_single_measure_control_chart():
-    # TO BE IMPLEMENTED
-    return
+def plot_single_measure_control_chart(series, x_label=None,
+                                      y_label=None, title=None,
+                                      ax=None, show=True, save=False,
+                                      **kwargs):
+    
+    _, x_bar, LCL, UCL = single_measure_control_chart(series)
+
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10, 6),
+                               gridspec_kw={"left": 0.07,
+                                            "right": 0.87,
+                                            "bottom": 0.1,
+                                            "top": 0.94})
+
+    indices = [x+1 for x in range(len(series))]
+    labels = [str(i) for i in indices]
+    ax.plot(indices, series, **kwargs)
+    ax.set_xticks(indices)
+    ax.set_xticklabels(labels)
+
+    ax.axhline(y=x_bar)
+    ax.annotate("Mean={:.3f}".format(x_bar), xy=(1.01, x_bar),
+                xycoords=("axes fraction", "data"))
+
+    ax.axhline(y=LCL)
+    ax.annotate("LCL={:.3f}".format(LCL), xy=(1.01, LCL),
+                xycoords=("axes fraction", "data"))
+
+    ax.axhline(y=UCL)
+    ax.annotate("UCL={:.3f}".format(UCL), xy=(1.01, UCL),
+                xycoords=("axes fraction", "data"))
+
+    ax.set_title(title)
+
+    if x_label is None:
+        x_label = "No."
+
+    ax.set_xlabel(x_label)
+
+    if y_label is None:
+        y_label = "Measure"
+
+    ax.set_ylabel(y_label)
+
+    show_and_save_plot(show=show, save=save,
+                       filename="single_measure_control_chart.png")
 
 
 # TESTING
@@ -307,42 +390,54 @@ def test_plot_run_chart():
     df, target, LSL, USL = load_input("../testing/fridge.dat")
     all_values = df.to_numpy().ravel()
     plot_run_chart(all_values, LSL, USL, y_label="Thickness",
-                   title="Run Chart", ax=None, marker="o")
+                   title="Run Chart", marker="o")
 
 
 def test_plot_xbar_s_chart():
     df, _, _, _ = load_input("../testing/fridge.dat")
     plot_xbar_s_chart(df, group_axis="row", x_label=None, y_label=None,
-                      title="X-bar (S) Chart", ax=None)
+                      title="X-bar (S) Chart")
 
 
 def test_plot_xbar_r_chart():
     df, _, _, _ = load_input("../testing/fridge.dat")
     plot_xbar_r_chart(df, group_axis="row", x_label=None, y_label=None,
-                      title="X-bar (R) Chart", ax=None, marker="o",
-                      c="g")
+                      title="X-bar (R) Chart", marker="o", c="g")
 
 
 def test_plot_r_chart():
     df, _, _, _ = load_input("../testing/fridge.dat")
     plot_r_chart(df, group_axis="row", x_label=None, y_label=None,
-                 title="R Chart", ax=None)
+                 title="R Chart")
 
 
 def test_plot_s_chart():
     df, _, _, _ = load_input("../testing/fridge.dat")
     plot_s_chart(df, group_axis="row", x_label=None, y_label=None,
-                 title="S Chart", ax=None)
+                 title="S Chart")
 
 
 def test_plot_moving_range_chart():
     df, _, _, _ = load_input("../testing/fridge.dat")
     plot_moving_range_chart(df, group_axis="row", x_label=None,
-                            y_label=None, title="S Chart", ax=None)
+                            y_label=None, title="MR Chart", ax=None)
 
+
+def test_plot_p_chart():
+    df = pd.read_csv("../testing/defects.csv")
+    plot_p_chart("num_of_reworks", "total_production", df,
+                 title="p Chart")
+
+
+def test_plot_single_measure_control_chart():
+    df = pd.read_csv("../testing/complaints.csv")
+    plot_single_measure_control_chart(df["number_of_complaints"],
+                                      title="Control Chart",
+                                      marker=".")
+                                      
 
 if __name__ == "__main__":
-    test_plot_moving_range_chart()
+    test_plot_single_measure_control_chart()
 
 
 ##def capability_histogram(df, x_label="Measure", bins=10, LSL=None, USL=None,
